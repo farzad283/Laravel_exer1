9f5ead597c9ab2fb23c92c247ffcc6a46995eb6a
Farzad <farzad@Farzads-MacBook-Air.local>
2023-05-07 16:50:09 -0400

    exercice1

---
+++ exercice1/personal/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PersonalController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/rty', function () {
+    return view('welcome');
+});
+
+Route::get('/home', [PersonalController::class, 'index']); 
+Route::get('/resume', [PersonalController::class, 'resume']);
+Route::get('/projects', [PersonalController::class, 'projects']);
+
+Route::get('/contact', [PersonalController::class, 'contact']);
+Route::post('/contact', [PersonalController::class, 'contactForm']); 
